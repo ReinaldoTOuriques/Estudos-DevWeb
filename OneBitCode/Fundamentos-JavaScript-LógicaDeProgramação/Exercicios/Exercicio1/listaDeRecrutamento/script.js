32c40aa35dc33d1ea0262e7788c6ec2c8e369b6b
Reinaldo Takashima Ouriques <reinaldotouriques@gmail.com>
2023-05-11 17:59:50 -0300

    Ainda falta refatorar para que a idade seja mostrada corretamente, bem como omitir o campo outro caso não tenha sido utilizado.

--- OneBitCode/Fundamentos-JavaScript-LógicaDeProgramação/Exercicios/Exercicio1/listaDeRecrutamento/script.js
+++ OneBitCode/Fundamentos-JavaScript-LógicaDeProgramação/Exercicios/Exercicio1/listaDeRecrutamento/script.js
@@ -2,10 +2,32 @@
 let fieldOfStudySelect = document.getElementById("fieldOfStudy");
   let outroOption = document.getElementById("outroOption");
 
-  fieldOfStudySelect.addEventListener('change', function() {
-    if (fieldOfStudySelect.value === 'outro') {
-      outroOption.style.display = 'block';
-    } else {
-      outroOption.style.display = 'none';
-    }
-  });
+fieldOfStudySelect.addEventListener('change', function() {
+  if (fieldOfStudySelect.value === 'outro') {
+    outroOption.style.display = 'block';
+  } else {
+    outroOption.style.display = 'none';
+  }
+});
+
+// Seleciona o formulário e adiciona um evento de escuta para quando o formulário for enviado
+const formulario = document.getElementById("formTag");
+
+formulario.addEventListener('submit', function(event) {
+  event.preventDefault(); // Evita que a página seja recarregada ao enviar o formulário
+
+// Seleciona os campos do formulário e salva as informações em variáveis
+  const primeiroNome = document.getElementById("firstNameInput").value;
+  const sobrenome = document.getElementById("lastNameInput").value;
+  const email = document.getElementById("emailInput").value;
+  const campoDeEstudo = document.getElementById("fieldOfStudy").value;
+  const outroCampoDeEstudo = document.getElementById("outro").value
+  const anoDeNascimento = document.getElementById("birthYearInput").value;
+
+// Calcula a idade baseada no ano de nascimento
+  const idade = new Date().getFullYear() - anoDeNascimento;
+
+// Exibe as informações na tela.
+  alert(`Nome completo: ${primeiroNome} ${sobrenome}\nE-mail: ${email}\nCampo de estudo: ${campoDeEstudo}\nCampo de estudo (outro): ${outroCampoDeEstudo}\nIdade: ${idade}`);
+
+});
