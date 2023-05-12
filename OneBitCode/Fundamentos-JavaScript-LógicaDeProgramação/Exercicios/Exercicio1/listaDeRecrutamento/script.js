5bc90e23382d4b68ba8c561e26ef706a3c0d47d9
Reinaldo Takashima Ouriques <reinaldotouriques@gmail.com>
2023-05-12 10:20:53 -0300

    Melhorias no core do código do projeto e adiciona estilo à página com CSS3 simples.

--- OneBitCode/Fundamentos-JavaScript-LógicaDeProgramação/Exercicios/Exercicio1/listaDeRecrutamento/script.js
+++ OneBitCode/Fundamentos-JavaScript-LógicaDeProgramação/Exercicios/Exercicio1/listaDeRecrutamento/script.js
@@ -1,8 +1,9 @@
-// Função para mostrar input ao usuário casos seja necessário inserir manualmente o campo de estudo.
-let fieldOfStudySelect = document.getElementById("fieldOfStudy");
-  let outroOption = document.getElementById("outroOption");
 
-fieldOfStudySelect.addEventListener('change', function() {
+const fieldOfStudySelect = document.getElementById("fieldOfStudy");
+const outroOption = document.getElementById("outroOption");
+
+// Função para mostrar input ao usuário casos seja necessário inserir manualmente o campo de estudo.
+fieldOfStudySelect.addEventListener('change', () => {
   if (fieldOfStudySelect.value === 'outro') {
     outroOption.style.display = 'block';
   } else {
@@ -10,24 +11,34 @@ fieldOfStudySelect.addEventListener('change', function() {
   }
 });
 
-// Seleciona o formulário e adiciona um evento de escuta para quando o formulário for enviado
+// Seleciona o formulário através do DOM.
 const formulario = document.getElementById("formTag");
 
+// Adiciona um evento de escuta para quando o formulário for enviado e evita que a página seja recarregada.
 formulario.addEventListener('submit', function(event) {
-  event.preventDefault(); // Evita que a página seja recarregada ao enviar o formulário
+  event.preventDefault();
 
-// Seleciona os campos do formulário e salva as informações em variáveis
-  const primeiroNome = document.getElementById("firstNameInput").value;
-  const sobrenome = document.getElementById("lastNameInput").value;
-  const email = document.getElementById("emailInput").value;
-  const campoDeEstudo = document.getElementById("fieldOfStudy").value;
-  const outroCampoDeEstudo = document.getElementById("outro").value
-  const anoDeNascimento = document.getElementById("birthYearInput").value;
+// Seleciona os campos do formulário e salva as informações em variáveis por meio do "().value" para serem utilizadas por meio do método alert() e apresentá-las ao usuário.
+const fieldOfStudyValue = document.getElementById("fieldOfStudy").value;
+const outroOptionValue = document.getElementById("outro").value;
+const primeiroNome = document.getElementById("firstNameInput").value;
+const sobrenome = document.getElementById("lastNameInput").value;
+const email = document.getElementById("emailInput").value;
+const anoDeNascimento = document.getElementById("birthYearInput").value;
 
-// Calcula a idade baseada no ano de nascimento
-  const idade = new Date().getFullYear() - anoDeNascimento;
-
-// Exibe as informações na tela.
-  alert(`Nome completo: ${primeiroNome} ${sobrenome}\nE-mail: ${email}\nCampo de estudo: ${campoDeEstudo}\nCampo de estudo (outro): ${outroCampoDeEstudo}\nIdade: ${idade}`);
-
+// De acordo com o que o usuário selecionar no "Campo de estudo", exibe as informações na tela.
+  if(fieldOfStudySelect.value === "outro") {
+    alert(`Nome completo: ${primeiroNome} ${sobrenome}\n
+    E-mail: ${email}\n
+    Campo de estudo (outro): ${outroOptionValue}\n
+    Idade: ${anoDeNascimento}`);
+  } else {
+    alert(`Nome completo: ${primeiroNome} ${sobrenome}\n
+    E-mail: ${email}\n
+    Campo de estudo: ${fieldOfStudyValue}\n
+    Idade: ${anoDeNascimento}`);
+  }
 });
+
+// função que calcula a idade baseada no ano de nascimento, conforme solicitado no exercício. Porém, devido ao fato de eu ter escolhido mudar um pouco o projeto, esse requisito não se encaixa no meu design de página.
+/* const idade = new Date().getFullYear() - anoDeNascimento; */
