322ee0911e664b3c947da1f2fa82d24765aa94e3
Reinaldo Takashima Ouriques <reinaldotouriques@gmail.com>
2023-05-15 14:30:03 -0300

    Desenvolve a função que verifica o dano causado pelo personagem atacante. Precisa melhorar o código ainda.

--- OneBitCode/Fundamentos-JavaScript-LógicaDeProgramação/Exercicios/Exercicio2/calculoDeDano/script.js
+++ OneBitCode/Fundamentos-JavaScript-LógicaDeProgramação/Exercicios/Exercicio2/calculoDeDano/script.js
@@ -12,12 +12,18 @@ form.addEventListener('submit', function(event) {
   const defenderHealth = document.getElementById('defenderHealth').value;
   const defenderDefense = document.getElementById('defenderDefense').value;
   const defenderShield = document.getElementById('defenderShield').checked;
-
-  // Displaying the user input in the console
-  console.log('Attacker Name:', attackerName);
-  console.log('Attacker Attack Power:', attackerAttack);
-  console.log('Defender Name:', defenderName);
-  console.log('Defender Hit Points:', defenderHealth);
-  console.log('Defender Defense Power:', defenderDefense);
-  console.log('Defender Shield:', defenderShield);
+  
+  // Through the conditional structure, it checks how much damage the attacking character managed to deal to the defender, based on the business rules.
+  let damageDealt = 0
+  if(attackerAttack > defenderDefense && !defenderShield) {
+    damageDealt = attackerAttack - defenderDefense;
+  } else {
+    damageDealt = Math.floor((attackerAttack - defenderDefense) / 2);
+  }
 });
+
+const newDefendersHealth = Math.max(defenderHealth - damageDealt, 0);
+
+const resultDiv = document.querySelector("#result");
+resultDiv.innerHTML = `${attackerName} attacked ${defenderName} and dealt ${damage} damage.<br>`;
+resultDiv.innerHTML += `${defenderName} now has ${newDefenderHealth} hit points remaining.`;
