7278992864be3744b58994eb45c12541f830d742
Reinaldo Takashima Ouriques <reinaldotouriques@gmail.com>
2023-05-19 10:47:35 -0300

    Chore: I solved the problem by declaring a new variable so that the user could select the measurement to which he would like to convert and changed the logic of the switch case so that, according to the user's choice, the page would send an alert with the result of conversion.

--- OneBitCode/Fundamentos-JavaScript-LógicaDeProgramação/Exercicios/Exercicio3/script.js
+++ OneBitCode/Fundamentos-JavaScript-LógicaDeProgramação/Exercicios/Exercicio3/script.js
@@ -1,4 +1,4 @@
-const measuerInput = prompt("enter the measurement in meters that you want to convert.");
+const measuerInput = prompt("Enter the measurement in meters that you want to convert.");
 const meters = parseFloat(measuerInput);
 const millimeters = meters * 10000;
 const centimeters = meters * 100;
@@ -6,27 +6,28 @@ const decimeters = meters * 10;
 const dekamteres = meters / 10;
 const hectometer = meters / 100;
 const kilometers = meters / 1000;
+const selectMeasurementTypeForConversion = prompt("Select measurement type for conversion: millimeters, centimeters, decimeters, dekamteres, hectometers, kilometers");
 
-switch (true) {
-  case meters === millimeters:
+switch (selectMeasurementTypeForConversion) {
+  case "millimeters":
     alert(millimeters + " millimeters");
     break;
-  case meters === centimeters:
+  case "centimeters":
     alert(centimeters + " centimeters");
     break;
-  case meters === decimeters:
+  case "decimeters":
     alert(decimeters + " decimeters");
     break;
-  case meters === dekamteres:
+  case "dekamteres":
     alert(dekamteres + " dekamteres");
     break;
-  case meters === hectometer:
+  case "hectometers":
     alert(hectometer + " hectometers");
     break;
-  case meters === kilometers:
+  case "kilometers":
     alert(kilometers + " kilometers");
     break;
   default:
-    alert("invalid measurement");
+    alert("Invalid measurement type");
     break;
 }
